c384df39b2917d4148734feadc16c45b6470d4e7
kirsten <lee.kirstie41@gmail.com>
2015-01-20 10:03:03 -0600

    more robustness to map and ability to add markers

--- app/src/main/java/com/example/user/lstapp/MapFragment.java
+++ app/src/main/java/com/example/user/lstapp/MapFragment.java
@@ -16,15 +16,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-import 	android.graphics.Color;
+import com.google.android.gms.maps.model.*;
+import android.graphics.Color;
 
 
 
@@ -36,7 +34,7 @@ import 	android.graphics.Color;
  * Use the {@link MapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapFragment extends Fragment implements OnMarkerClickListener {
+public class MapFragment extends Fragment implements OnMarkerClickListener, OnMapClickListener {
 
     private static View view;
     /**
@@ -59,12 +57,25 @@ public class MapFragment extends Fragment implements OnMarkerClickListener {
         // Passing harcoded values for latitude & longitude. Please change as per your need. This is just used to drop a Marker on the Map
         latitude = 26.78;
         longitude = 72.56;
-        //FragmentManager fm = getSupportFragmentManager();
-        //Context context = (Context) getActivity();
-        //FragmentManager fm = getActivity().getSupportFragmentManager();
+
         FragmentManager fm = getChildFragmentManager();
 
         setUpMapIfNeeded(fm); // For setting up the MapFragment
+
+        if (mMap != null) {
+            try {
+                mMap.setOnMarkerClickListener((OnMarkerClickListener) this);
+            } catch (ClassCastException e) {
+                throw new ClassCastException("map must implement OnMarkerClickListener");
+            }
+
+            try{
+                mMap.setOnMapClickListener((OnMapClickListener) this);
+            }catch (ClassCastException e) {
+                throw new ClassCastException("map must implement OnMapClickListener");
+            }
+            setUpMap();
+        }
 
         return view;
     }
@@ -94,17 +105,34 @@ public class MapFragment extends Fragment implements OnMarkerClickListener {
         // For showing a move to my loction button
         mMap.setMyLocationEnabled(true);
         // For dropping a marker at a point on the Map
-        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("My Home").snippet("Home Address"));
+        //Marker pt = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude,
+        //        longitude)).snippet("Home Address"));
         // For zooming automatically to the Dropped PIN Location
-        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,
-        //        longitude), 12.0f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,
+                longitude), 12.0f));
+    }
+
+    public void drawRangeMarker(Marker marker){
+        LatLng center = marker.getPosition();
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(center)
+                .radius(100)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
     }
 
     public boolean onMarkerClick(Marker marker){
+        //TODO: call LST functions to show pok of region
+        //drawRangeMarker(marker);
         return true;
     }
 
-    public boolean drawTest(){
+    public void onMapClick(LatLng point){
+        Marker pt = mMap.addMarker(new MarkerOptions().position(point).snippet("Pok: TODO"));
+        drawRangeMarker(pt);
+    }
+
+    public boolean drawTest(){ //EXAMPLE: of drawing on map, TODO
         Polygon polygon = mMap.addPolygon(new PolygonOptions()
                 .add(new LatLng(0, 0), new LatLng(0, 5), new LatLng(3, 5), new LatLng(0, 0))
                 .strokeColor(Color.RED)
@@ -114,7 +142,6 @@ public class MapFragment extends Fragment implements OnMarkerClickListener {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         if (mMap != null)
             setUpMap();
 
@@ -123,14 +150,6 @@ public class MapFragment extends Fragment implements OnMarkerClickListener {
             mMap = ((SupportMapFragment) getActivity().getSupportFragmentManager()
                     .findFragmentById(R.id.location_map)).getMap();
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                try {
-                    mMap.setOnMarkerClickListener((OnMarkerClickListener) this);
-                } catch (ClassCastException e) {
-                    throw new ClassCastException(" must implement OnMarkerClickListener");
-                }
-                setUpMap();
-            }
         }
     }
 
@@ -145,10 +164,5 @@ public class MapFragment extends Fragment implements OnMarkerClickListener {
             //        .remove(getActivity().getSupportFragmentManager().findFragmentById(R.id.location_map));//.commit();
             //mMap = null;
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 }
